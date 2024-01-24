f27a706d8996adf39826bbb34438c98d538f6656
Elinaldo Lopes de Castro Paes <elinaldolopesc@gmail.com>
2024-01-24 16:35:31 -0300

    ex015

---
+++ JS/exercícios/ex015/script.js
@@ -0,0 +1,32 @@
+function contar() {
+    var In = document.getElementById("in")
+    var fi = window.document.getElementById("fi")
+    var pa = document.getElementById("pa")
+    var res = window.document.getElementById("res")
+
+
+    if (In.value.length == 0 || fi.value.length == 0 || pa.value.length == 0) {
+        res.innerHTML = "impossivel contar..."
+
+
+    } else if (Number(pa.value) == 0) {
+        alert("Passo invalido cosiderando passo 1")
+        var ni = Number(In.value)
+        res.innerHTML = "contando: <br>"
+        for (ni ; ni <= Number(fi.value) ; ni += 1) {
+        res.insertAdjacentText('beforeend', ` ${ni} ➡️`)
+        }
+        res.insertAdjacentText('beforeend', `🚩`)
+
+
+    } else {    
+        var ni = Number(In.value)
+        res.innerHTML = "contando: <br>"
+        for (ni ; ni <= Number(fi.value) ; ni += Number(pa.value)) {
+        res.insertAdjacentText('beforeend', ` ${ni} ➡️`)
+        }
+        res.insertAdjacentText('beforeend', `🚩`)
+    }
+    
+    
+}
